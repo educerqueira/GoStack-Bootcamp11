3e72b185ee94a9efd9bc27adb283ed1f3327d151
eduardo.cerqueira <eduardo.cerqueira@vamosparcelar.com.br>
2022-01-17 14:02:08 -0300

    Adiciona arquivos para configurar projeto

---
+++ backend/src/index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+
+const app = express(); 
+
+/**
+ * Métodos HTTTP:
+ * 
+ * GET: Buscar informações do back-end
+ * POST: Criar uma informação no back-end
+ * PUT/PATCH: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Filtros e paginação
+ * Route Params: Identificar recursos (Atualizar/Deletar)
+ * Request Body:
+ */
+
+app.get('/projects', (request, response)=>{
+  const {title, owner} = request.query;
+
+  console.log(title);
+  console.log(owner);
+
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
+});
+
+app.post('/projects', (request, response)=>{
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3'
+  ]);
+});
+
+app.put('/projects/:id', (request, response)=>{
+  return response.json([
+    'Projeto 4',
+    'Projeto 2',
+    'Projeto 3'
+  ]);
+});
+
+app.delete('/projects/:id', (request, response)=>{
+  return response.json([
+    'Projeto 2',
+    'Projeto 3'
+  ]);
+});
+
+app.listen(3333, ()=>{
+  console.log('🚀 Back-end started!')
+}); 
